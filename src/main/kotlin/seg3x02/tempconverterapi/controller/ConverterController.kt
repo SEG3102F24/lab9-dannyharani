2a654b51581e7c85169750d15a16ea7fb3f2c72a
Danny <danniharany@gmail.com>
2024-11-17 14:01:21 -0500

    Final submission

--- src/main/kotlin/seg3x02/tempconverterapi/controller/ConverterController.kt
+++ src/main/kotlin/seg3x02/tempconverterapi/controller/ConverterController.kt
@@ -1,16 +1,58 @@
 package seg3x02.tempconverterapi.controller
 
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.ModelAttribute
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.RequestParam
 
-@RestController
-@RequestMapping("temperature-converter")
+@Controller
 class ConverterController {
-    @GetMapping("/celsius-fahrenheit/{celsius}")
-    fun getFahrenheit(@PathVariable celsius: Double) = ((celsius * 9) / 5 + 32)
+    @ModelAttribute
+    fun addAttributes(model: Model) {
+        model.addAttribute("error", "")
+        model.addAttribute("celsius", "")
+        model.addAttribute("fahrenheit", "")
+    }
 
-    @GetMapping("/fahrenheit-celsius/{fahrenheit}")
-    fun getCelsius(@PathVariable fahrenheit: Double) = ((fahrenheit - 32) * 5) / 9
+    @RequestMapping("/")
+    fun home(): String {
+        return "home"
+    }
+
+    @GetMapping(value = ["/convert"])
+    fun doConvert(
+        @RequestParam(value = "celsius", required = false) celsius_input: String,
+        @RequestParam(value = "fahrenheit", required = false) fahrenheit_input: String,
+        @RequestParam(value = "operation", required = false) operation: String,
+        model: Model
+    ): String {
+        val celsiusResult: Double
+        val fahrenheitResult: Double
+        when (operation) {
+            "CelsiusToFarenheit" ->
+                try {
+                    celsiusResult = celsius_input.toDouble()
+                    fahrenheitResult = ((celsiusResult * 9) / 5 + 32)
+                    model.addAttribute("celsius", celsius_input)
+                    model.addAttribute("fahrenheit", String.format("%.2f", fahrenheitResult))
+                } catch (exp: NumberFormatException) {
+                    model.addAttribute("error", "CelsiusFormatError")
+                }
+            "FarenheitToCelsius" ->
+                try {
+                    fahrenheitResult = fahrenheit_input.toDouble()
+                    celsiusResult = ((fahrenheitResult - 32) * 5) / 9
+                    model.addAttribute("celsius", String.format("%.2f", celsiusResult))
+                    model.addAttribute("fahrenheit", fahrenheit_input)
+                } catch (exp: NumberFormatException) {
+                    model.addAttribute("error", "FahrenheitFormatError")
+                }
+            else -> {
+                model.addAttribute("error", "OperationFormatError")
+            }
+        }
+        return "home"
+    }
 }
